e96b5da2b0136c5e64f9b80a294fbefa9d1cbf24
Harmanjeet <33647824+blessedbrar@users.noreply.github.com>
2022-07-06 11:24:14 -0700

    added new files to project
    Calculator
    Fibonnaci
    Sleep

---
+++ Project1/Calculator.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var operator string
+	var number1, number2 int
+
+	fmt.Println("Please enter the First number: ")
+	fmt.Scanln(&number1)
+	fmt.Println("Please enter the Second number: ")
+	fmt.Scanln(&number2)
+	fmt.Println("Please enter Operator (+,-,/,%,*):")
+	fmt.Scanln(&operator)
+	output := 0
+
+	switch operator {
+	case "+":
+		output = number1 + number2
+		break
+	case "-":
+		output = number1 - number2
+	case "*":
+		output = number1 * number2
+	case "/":
+		output = number1 / number2
+	case "%":
+		output = number1 % number2
+	default:
+		fmt.Println("Invalid Operation")
+	}
+	fmt.Printf("%d %s %d =%d", number1, operator, number2, output)
+}
